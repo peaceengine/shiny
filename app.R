43b047175e3e901d96aa8af99d431443c1884cfa
Antony Butts <antonybutts@me.com>
2023-07-13 14:39:16 +0100

    Add files via upload

--- app.R
+++ app.R
@@ -1,59 +1,83 @@
-# Load required libraries
+# Required libraries
 library(shiny)
+library(igraph)
 
-# Define UI for application
+# Define UI
 ui <- fluidPage(
-  # Create dropdown menu for selecting burger ingredients
-  selectizeInput("ingredients", "Choose your ingredients", 
-                 choices = c("Ketchup", "Gherkins", "Cheese"),
-                 multiple = TRUE),
-  
-  # Create a placeholder for UI elements
-  uiOutput("ui_elements"),
-  
-  # Show selected ingredients
-  verbatimTextOutput("selected_items")
+  titlePanel("Network Node and Edge Adder"),
+  sidebarLayout(
+    sidebarPanel(
+      actionButton("add_node", "Add Node"),
+      actionButton("add_edge_modal", "Add Edge"),
+      actionButton("create_edge_list", "Create Edge List")
+    ),
+    mainPanel(
+      plotOutput("networkPlot"),
+      tableOutput("edgeListTable")
+    )
+  )
 )
 
-# Define server logic required
+# Define server logic
 server <- function(input, output, session) {
-  # Create reactive variable for selected ingredients
-  selected_ingredients <- reactive({
-    input$ingredients
+  # Our reactive graph
+  rv <- reactiveValues(graph = g)
+  
+  # Add node modal
+  observeEvent(input$add_node, {
+    showModal(modalDialog(
+      title = "Add a Node",
+      textInput("node_name", "Name of the Node"),
+      radioButtons("node_time", "Select Time", choices = c('A', 'B', 'C')),
+      radioButtons("node_type", "Select Type", choices = c('Trend', 'Event')),
+      easyClose = FALSE,
+      footer = tagList(
+        modalButton("Cancel"),
+        actionButton("save_node", "Add Node")
+      )
+    ))
   })
   
-  # Create UI elements for removing ingredients
-  output$ui_elements <- renderUI({
-    if (!is.null(selected_ingredients())) {
-      tagList(lapply(selected_ingredients(), function(ingredient) {
-        div(style = "display: inline-block; padding: 1px; margin: 1px",
-            wellPanel(
-              style = "padding: 5px; margin: 0px;", 
-              ingredient,
-              actionButton(paste0("remove_", gsub(" ", "", ingredient)), "X")
-            )
-        )
-      }))
-    }
+  observeEvent(input$save_node, {
+    newNode <- input$node_name
+    rv$graph <- add_vertices(rv$graph, 1, name = newNode, time = input$node_time, type = input$node_type)
+    updateSelectInput(session, "from_node", choices = V(rv$graph)$name)
+    updateSelectInput(session, "to_node", choices = V(rv$graph)$name)
+    removeModal()
   })
   
-  # Show selected ingredients
-  output$selected_items <- renderPrint({
-    selected_ingredients()
+  # Add edge modal
+  observeEvent(input$add_edge_modal, {
+    showModal(modalDialog(
+      title = "Add an Edge",
+      selectInput("from_node_modal", "Select From Node:", choices = V(rv$graph)$name),
+      selectInput("to_node_modal", "Select To Node:", choices = V(rv$graph)$name),
+      numericInput("edge_weight", "Weight:", value = 1, min = 0),
+      easyClose = FALSE,
+      footer = tagList(
+        modalButton("Cancel"),
+        actionButton("save_edge", "Add Edge")
+      )
+    ))
   })
   
-  # Observe for any remove action
-  observe({
-    if (!is.null(selected_ingredients())) {
-      lapply(selected_ingredients(), function(ingredient) {
-        observeEvent(input[[paste0("remove_", gsub(" ", "", ingredient))]], {
-          updateSelectizeInput(session, "ingredients", 
-                               selected = setdiff(selected_ingredients(), ingredient))
-        }, ignoreNULL = FALSE, ignoreInit = TRUE)
-      })
-    }
+  observeEvent(input$save_edge, {
+    rv$graph <- add_edges(rv$graph, c(input$from_node_modal, input$to_node_modal))
+    rv$graph <- set_edge_attr(rv$graph, "weight", index = E(rv$graph)[length(E(rv$graph))], value = input$edge_weight)
+    removeModal()
+  })
+  
+  # Create edge list
+  observeEvent(input$create_edge_list, {
+    edge_list <- get.data.frame(rv$graph, what = "edges")
+    output$edgeListTable <- renderTable(edge_list)
+  })
+  
+  output$networkPlot <- renderPlot({
+    plot.igraph(rv$graph, edge.arrow.size=.5, vertex.label.dist=1, vertex.size=5, vertex.frame.color="#777777",
+                vertex.label.color="#777777", vertex.label.cex=0.75, vertex.label.family="sans")
   })
 }
 
-# Run the application
+# Run the application 
 shinyApp(ui = ui, server = server)
